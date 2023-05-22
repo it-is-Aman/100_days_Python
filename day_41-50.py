d92288c07a7969da4a1d366dd7279d4c3731583e
Aman <rajputaman2003@outlook.com>
2023-05-22 18:43:12 +0530

    Day 41 to 50

---
+++ day_41-50.py
@@ -0,0 +1,27 @@
+## 41 - If ... Else in One Line
+# result = value_if_true if condition else value_if_false
+
+# a=7
+# b=3
+# print("Nice") if a>b else print("not cool") if a==b else ""     #have to write else "" in end
+
+# oldVal = 2
+# cond=6
+# newVal = 7 if cond > 5 else 9   #newVal will be 7 'cause cond > 5
+# print(newVal)
+
+
+## 42 - Enumerate function 
+# allows you to loop over a sequence (such as a list, tuple, or string) and get the index and value of each element in the sequence at the same time
+
+# lstName = ["wsd","cool","nice","boy"]
+# for index,name in enumerate(lstName):
+#     print(index, name)  #Loop over a tuple and print the index and value of each element
+
+# lstName = ["wsd","cool","nice","boy"]
+# for index,name in enumerate(lstName, start=10): #index will start from 10
+#     print(index, name)  
+
+
+
+
