2137d068ef476e8452cd5391f0a3d254012c0549
Aman <rajputaman2003@outlook.com>
2023-05-26 16:12:55 +0530

    added some comments

--- MyFile.py
+++ MyFile.py
@@ -3,7 +3,7 @@ def welMsg():
 
 secret="You are AWESOME"
 
-print(__name__)     # Python script is run directly, the __name__ variable is set to the string __main__
-                    #When the script is imported as a module into another script, the __name__ variable is set to the name of the module
+print(__name__)     #When Python script is run directly, the __name__ variable is set to the string __main__
+                    #When the script is imported as a module into another script, the __name__ variable is set to the name of the module which is "MyFile"
 if __name__=="__main__":    #if i did't use this then, whenever i import this module 'MyFile' welMsg() will execute
     welMsg()
