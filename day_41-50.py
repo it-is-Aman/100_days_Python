14a28c1211f42d6c14d37663bc7208fbfc3ee8dc
Aman <rajputaman2003@outlook.com>
2023-05-26 16:18:12 +0530

    learned os module, made encoding and decoding of string program

--- day_41-50.py
+++ day_41-50.py
@@ -74,25 +74,78 @@ the script is being run directly or being imported as a module into another scri
 In Python, the __name__ variable is a built-in variable that is automatically set to the name of
 the current module. When a Python script is run directly, the __name__ variable is set to the
 string __main__ When the script is imported as a module into another script, the __name__ variable
-is set to the name of the module.
+is set to the name of the module
 
 This can be useful if you have code that you want to reuse in multiple scripts, but you only want it to run when the script is run directly and not when it's imported as a module.
 '''
 
-# import MyFile
+# import MyFile       #print "MyFile" because script is imported as a module into another script 
 # MyFile.welMsg()     #message print only onces because if __name__ == "__main__" idiom used
 
 
-## 46
+## 46 os Module in Python
+
+# import os
+
+# if(not os.path.exists("randomDir")):    #if directory not present then make
+#     print("directory has been created")
+#     os.mkdir("randomDir")   #make the directory with name "randomDir"
+
+# # for num in range(1,11,1):
+# #     os.mkdir(f"randomDir/random {num}")   #make the directory in bulk via loop 
+
+# # for num in range(1,11,1):
+# #     os.rename(f"randomDir/random {num}", f"randomDir/random_new {num}")     #rename the directory os.rename(old_name,new_name)
+
+# dirContent=os.listdir("randomDir")      #get a list of the files in a directory "randomDir"
+# print(dirContent)
+
+# ## file=os.open("revise.py",os.O_RDONLY)   # Open the file in read-only mode
+# ## content=os.read(file,10)      # Read the contents of the file with length
+# ## print(content)
+# ## os.close(file)      # Close the file
+# #
+# ## file=os.open("checking_oswrite.txt",os.O_WRONLY)   # Open the file in write-only mode
+# ## os.write(file,b"#Changed the context though day_41-50.py")      # write to the file
+# ## print("successfully written on checking_oswrite.txt")
+# ## os.close(file)      # Close the file
 
 
+## 47 Again Encoding and Decoding exercise
+'''
+String.ascii_letters 	It returns a random string that contains both uppercase and lowercase characters.
+String_ascii_uppercase 	It is a random string method that only returns a string in uppercase characters.
+String.ascii_lowercase 	It is a random string method that returns a string only in lowercase characters.
+String.digits 	It is a random string method that returns a string with numeric characters.
+String.punctuation 	It is a random string method that returns a string with punctuation characters.
 
+    random.choice(string.ascii_letters)     #must have both string,random module and generate random string
+'''
 
-
-
-
-
-
+# import random,string
+# str=input("Enter your string: ")
+# con=int(input("1- Encoding, 2- Decoding: "))
+# ranString=""        #empty string
+# cond=True if con==1 else False
+# if cond==True:
+#     if(len(str)>=3):
+#         for i in range(1,4,1):      #loop generate random char and store in ranString only 3 characters
+#             ranString+=random.choice(string.ascii_lowercase)
+#         str=ranString + str[1:] + str[0] + ranString        
+#         str=str[::-1]       #this will reverse the string
+#         print("Encoded message: ",str)
+#     else:
+#         str=str[::-1]
+#         print("Encoded message: ",str)
+# if cond==False:
+#     if(len(str)>=3):
+#         str=str[::-1]       #reverse the string  
+#         str=str[3:-3]       #remove that ranString characters
+#         str=str[-1]+str[:-1]        #move last char into front
+#         print("Decoded message: ",str)
+#     else:
+#         str=str[::-1]
+#         print("Decoded message: ",str)
 
 
 
