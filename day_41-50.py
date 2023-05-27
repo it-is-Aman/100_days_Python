7763f751f415dc012757d998c11e662d5e549c71
Aman <rajputaman2003@outlook.com>
2023-05-27 17:36:34 +0530

    learned local & global variables, File I/O in Python

--- day_41-50.py
+++ day_41-50.py
@@ -149,11 +149,53 @@ String.punctuation 	It is a random string method that returns a string with punc
 
 
 
+## 48 Local and global variables
+
+# gA=7    #global variable
+# print(gA)
+
+# def f():
+#     global gA   #global keyword for modify global variable
+#     gA= 9   #this will change the value of the global variable gA
+#     lA=2    #local variable
+#     print('''local variable''',lA)
+
+# f()
+# print(gA)
+# # print(lA)   #error because lA is a local variable and is not accessible outside of the function
 
 
+## 49 File I/O in Python
+'''
+By default, the open() function returns a file object that can be used to read or write
 
+Modes in file
+There are various modes in which we can open files.
+    read (r): This mode opens the file for reading only and gives an error if the file does not exist. This is the default mode if no mode is passed as a parameter.
+    write (w): This mode opens the file for writing only and creates a new file if the file does not exist.
+    append (a): This mode opens the file for appending only and creates a new file if the file does not exist.
+    create (x): This mode creates a file and gives an error if the file already exists.
+    text (t): Apart from these modes we also need to specify how the file must be handled. t mode is used to handle text files. t refers to the text mode. There is no difference between r and rt or w and wt since text mode is the default. The default mode is 'r' (open for reading text, synonym of 'rt' ).
+    binary (b): used to handle binary files (images, pdfs, etc).
+'''
+# fl=open("file_io.txt","r")
+# fl_content=fl.read()
+# print(fl_content)
+# fl.close()
 
+# fl=open("file_io.txt","w")
+# fl.write("this is the modified statement\n")
+# fl.close()
 
+# fl=open("file_io.txt","a")
+# fl.write("this is the append statement\n")
+# fl.close()
 
+# with open("file_io.txt","r") as fl:     #'with' statement automatically close the file after you are done with it.
+#     print(fl.read())
 
+# with open("file_io.txt","w") as fl:
+#     fl.write("this is the modified statement\n")
 
+# with open("file_io.txt","a") as fl:
+#     fl.write("this is the append statement\n")
