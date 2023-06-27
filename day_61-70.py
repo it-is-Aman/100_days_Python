0d0ee1b63e5e2d2340960ff5003ced550f6b985f
Aman <rajputaman2003@outlook.com>
2023-06-27 18:43:28 +0530

    learned Access Specifiers/Modifiers

--- day_61-70.py
+++ day_61-70.py
@@ -1,6 +1,13 @@
 ## 61 - Inheritance in python
 '''
 When a class derives from another class. it can have its own properties and methods,this is called as inheritance.
+Types of inheritance:
+
+    Single inheritance
+    Multiple inheritance
+    Multilevel inheritance
+    Hierarchical Inheritance
+    Hybrid Inheritance
 syntax ->
 class BaseClass:
   Body of base class
@@ -26,4 +33,30 @@ class DerivedClass(BaseClass):
 # obj2.adv_st()
 
 
-## 62
+## 62  Access Specifiers/Modifiers
+'''
+    Public access modifier -> All the variables and methods (member functions) in python are by default public
+    Private access modifier -> prefix its name with a double underscore (__)Private members of a class (variables or methods) are those members which are only accessible inside the class. We cannot use private members outside of class.
+            Name mangling in Python is a technique to access private variable, we can access private attributes by calling _MyClass__"attribute_name"
+    Protected access modifier ->  a member is protected is to prefix its name with a single underscore (_).
+    '''
+
+# class Fruit:
+#     def pubName(self,value:str):
+#         self.name=value     #public variable
+#         print(f"fruit name is {self.name}")
+    
+#     def proName(self,value:str):      
+#         self.__word=value   #An indication of private variable
+#         print(f"fruit name is {self.__word}")
+
+#     def _proVar(self):  # protected method
+#         return "haha protected string"
+
+# obj=Fruit()
+# obj.pubName("banana")   
+# print(obj.name)     #we can accessed public variable outside of class
+# obj.proName("apple")    #we assign private variable some value
+# # print(obj.__word)     #give error cause we are accessing private variable so we have to use name mangling
+# print(obj._Fruit__word)     # _Myclass_variablename
+# print(obj._proVar())    # calling by object of Fruit class, print protected method return statement
